156a61111bc206bc553ca65eb626213f463cbbda
Carstar360 <clee124@ecc-jaguar.csuchico.edu>
2019-09-23 16:38:20 -0700

    Finished Fortune

--- main.cpp
+++ main.cpp
@@ -9,13 +9,17 @@ int main()
   //declare variables
    string first = " ";
    string last = " ";
+   
+   cout<<first.at(0)<<endl;
+   cout<<last.at(0)<<endl;
+
   //get user inpu
   cout<<"What is your first name?: "<<endl;
   cin>>first;
   cout<<"What is your last name?: "<<endl;
   cin>>last;
 
-  cout<<"Welcome "<<first<<" "<<last<<", here is your fortune..."<<endl;
+  cout<<"Welcome "<<first.at(0)<<"."<<last.at(0)<<"., here is your fortune..."<<endl;
 
   //tell fortune
 
