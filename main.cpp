8bcc9c765c1b002569513e48da123f8d84475cc7
Carstar360 <clee124@ecc-jaguar.csuchico.edu>
2019-09-25 16:52:01 -0700

    Revised 10

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 //Authors: Carson Lee
 #include<iostream>
 #include<string>
+#include<cctype>
 
 using namespace std;
 
@@ -10,9 +11,11 @@ int main()
    string first = " ";
    string last = " ";
    int lucky;
+   int lastNumber = ' ';
    
+      
    
-  //get user inpu
+  //get user input
   cout<<"What is your first name?"<<endl;
   cin>>first;
   cout<<"What is your last name?"<<endl;
@@ -20,8 +23,37 @@ int main()
   
   cout<<"Welcome, "<<first.at(0)<<"."<<last.at(0)<<"., here is your fortune..."<<endl;
 
+  lastNumber = (last.length() - 1);
+
+  first[0] = tolower(first.at(0));
+  last[lastNumber] = tolower(last.at(lastNumber));
+
+
+
   //tell fortune
   cout<<"your lucky number is "<<first.length()<<endl;
 
+  if (first.at(0) == 'a' || first.at(0) == 'e' || first.at(0) == 'o' || first.at(0) == 'i' ||
+      first.at(0) == 'u')
+  {
+     cout<<"you are destined to be famous!"<<endl;
+  }
+   else
+
+  {
+     cout<<"you should keep a low profile."<<endl;
+  }
+
+
+  if (last.at(lastNumber) == 'a' || last.at(lastNumber) == 'e' || last.at(lastNumber) == 'o' ||
+      last.at(lastNumber) == 'u' || last.at(lastNumber) == 'i')
+  {
+     cout<<"you have already met your true love."<<endl;
+  }
+  
+  else
+  {
+  cout<<"have a good day!"<<endl;
+  }
   return 0;
 }
